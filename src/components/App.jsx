0f144962585d746ff46cace8a3f5644fd468c35b
Minwei Xu <faceswilliam@gmail.com>
2017-02-18 15:56:27 -0500

    style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
+
 import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import { RaisedButton, TextField, Paper } from 'material-ui';
 
 const centerStyle = {
   color: '#EEEEEE',
@@ -30,17 +31,23 @@ const App = () => (
     <div className="container-fluid">
       <div className="row" style={{ position: 'absolute', height: '100%', width: '100%' }}>
         <div style={centerStyle} className="col-xs-offset-1 col-xs-5 text-left">
-          <p style={{ fontSize: '80px', fontWeight: '800', marginBottom: '10px' }}>
+          <p style={{ fontSize: '70px', fontWeight: '800', marginBottom: '10px' }}>
             iota
           </p>
-          <p style={{ fontSize: '60px', marginTop: '0px' }}>
+          <p style={{ fontSize: '55px', marginTop: '0px' }}>
             Iota work for odd jobs
           </p>
         </div>
-        <div className="col-xs-6"
-          style={{ float: 'none', display: 'inline-block', verticalAlign: 'middle', paddingTop: '30%' }}>
+        <div className="col-xs-offset-1 col-xs-4"
+          style={{ textAlign: 'center', verticalAlign: 'middle', paddingTop: '15%' }}>
+          <Paper style={{ background: 'rgba(230, 230, 230, .5)', paddingTop: '50px', paddingTop: '100px', paddingBottom: '100px'}} zDepth="3">
+            <TextField hintStyle={{ color: '#666666'}} inputStyle={{ color: '#111111'}} hintText="Email" />
+            <br />
+            <TextField hintStyle={{ color: '#666666'}} inputStyle={{ color: '#111111'}} hintText="Password" />
+            <br />
             <RaisedButton label="Log in" style={{ marginRight: '12px' }} href="/login" />
             <RaisedButton label="Sign up" href="/signup" />
+          </Paper>
         </div>
       </div>
     </div>
